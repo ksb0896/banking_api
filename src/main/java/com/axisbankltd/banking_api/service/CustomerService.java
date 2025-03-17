1c565907e2a418aea72c0052825d9d70b59de206
kushal96 <kushal96@debian>
2025-03-17 23:29:10 +0530

    restructured++

--- src/main/java/com/axisbankltd/banking_api/service/CustomerService.java
+++ src/main/java/com/axisbankltd/banking_api/service/CustomerService.java
@@ -6,8 +6,12 @@ import com.axisbankltd.banking_api.model.Customer;
 
 public interface CustomerService {
 	Customer createCustomer(Customer customer);
+
 	Customer getCustomerById(Long id);
+
 	List<Customer> getAllCustomers();
+
 	Customer updateCustomer(Long id, Customer customer);
+
 	void deleteCustomer(Long id);
 }
